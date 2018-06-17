2a4d05a9165e26e19bd98129dba0153f88776160
Chen Liang <slimhigh@gmail.com>
2018-06-17 19:24:19 +0800

    Cover SAP corner case.

--- src/main/java/com/chenleon/algo/assignment/wordnet/SAP.java
+++ src/main/java/com/chenleon/algo/assignment/wordnet/SAP.java
@@ -14,26 +14,52 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
         AncestorFinder finder = new AncestorFinder(G, v, w);
         return finder.length();
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
         AncestorFinder finder = new AncestorFinder(G, v, w);
         return finder.ancestor();
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        validateVertices(v);
+        validateVertices(w);
         AncestorFinder finder = new AncestorFinder(G, v, w);
         return finder.length();
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        validateVertices(v);
+        validateVertices(w);
         AncestorFinder finder = new AncestorFinder(G, v, w);
         return finder.ancestor();
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        if (v < 0 || v >= G.V())
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (G.V()-1));
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertices(Iterable<Integer> vertices) {
+        if (vertices == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+        for (int v : vertices) {
+            if (v < 0 || v >= G.V()) {
+                throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (G.V()-1));
+            }
+        }
     }
 
     // do unit testing of this class
---
+++ src/test/java/com/chenleon/algo/assignment/wordnet/SAPTest.java
@@ -0,0 +1,51 @@
+package com.chenleon.algo.assignment.wordnet;
+
+import edu.princeton.cs.algs4.Digraph;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class SAPTest {
+    private Digraph G;
+    @Before
+    public void setUp() throws Exception {
+        G = new Digraph(25);
+        G.addEdge(1, 0);
+        G.addEdge(2, 0);
+        G.addEdge(3, 1);
+        G.addEdge(4, 1);
+        G.addEdge(5, 2);
+        G.addEdge(6, 2);
+        G.addEdge(7, 3);
+        G.addEdge(8, 3);
+        G.addEdge(9, 3);
+        G.addEdge(10, 5);
+        G.addEdge(11, 5);
+        G.addEdge(12, 5);
+        G.addEdge(13, 7);
+        G.addEdge(14, 7);
+        G.addEdge(15, 9);
+        G.addEdge(16, 9);
+        G.addEdge(17, 10);
+        G.addEdge(18, 10);
+        G.addEdge(19, 12);
+        G.addEdge(20, 12);
+        G.addEdge(21, 16);
+        G.addEdge(22, 16);
+        G.addEdge(23, 20);
+        G.addEdge(24, 20);
+    }
+
+    @Test
+    public void testSap() {
+        SAP sap = new SAP(G);
+        assertEquals(4, sap.length(13, 16));
+        assertEquals(3, sap.ancestor(13, 16));
+
+        assertEquals(4, sap.length(Arrays.asList(13, 23, 24), Arrays.asList(6, 16, 17)));
+        assertEquals(3, sap.ancestor(Arrays.asList(13, 23, 24), Arrays.asList(6, 16, 17)));
+    }
+}
