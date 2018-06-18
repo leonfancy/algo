ceb9c78681448b6c6f65b1b8d7e265450aa090bd
Chen Liang <slimhigh@gmail.com>
2018-06-18 22:40:38 +0800

    Implement WordNet.

---
+++ src/main/java/com/chenleon/algo/assignment/wordnet/WordNet.java
@@ -0,0 +1,77 @@
+package com.chenleon.algo.assignment.wordnet;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+
+import java.util.ArrayList;
+
+import static java.lang.Integer.parseInt;
+
+public class WordNet {
+    private ST<String, Bag<Integer>> wordsST;
+    private ArrayList<String> keys;
+    private Digraph G;
+
+    // constructor takes the name of the two input files linearithmic
+    public WordNet(String synsets, String hypernyms) {
+        if(synsets == null || hypernyms == null) throw new IllegalArgumentException();
+
+        wordsST = new ST<>();
+        keys = new ArrayList<>();
+
+        In in = new In(synsets);
+        int count = 0;
+        while(in.hasNextLine()) {
+            String[] a = in.readLine().split(",");
+            int id = parseInt(a[0]);
+            keys.add(id, a[1]);
+
+            String[] words = a[1].split(" ");
+            for (String word : words) {
+                Bag<Integer> ids = wordsST.get(word);
+                if(ids == null) ids = new Bag<>();
+
+                ids.add(id);
+            }
+
+            count++;
+        }
+
+        in = new In(hypernyms);
+        G = new Digraph(count);
+        while(in.hasNextLine()) {
+            String[] a = in.readLine().split(",");
+            int v = parseInt(a[0]);
+
+            for (int i = 1; i < a.length; i++)
+                G.addEdge(v, parseInt(a[i]));
+        }
+    }
+
+    // returns all WordNet wordsST
+    public Iterable<String> nouns() {
+        return wordsST.keys();
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        return wordsST.get(word) != null;
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+        if(!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+        AncestorFinder finder = new AncestorFinder(G, wordsST.get(nounA), wordsST.get(nounB));
+        return finder.length();
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB) {
+        if(!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+        AncestorFinder finder = new AncestorFinder(G, wordsST.get(nounA), wordsST.get(nounB));
+        return keys.get(finder.ancestor());
+    }
+}
