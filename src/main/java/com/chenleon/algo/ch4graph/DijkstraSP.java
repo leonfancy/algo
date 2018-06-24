b49ecfaa0468661c13aea6e7939c6ba531fa0412
Chen Liang <slimhigh@gmail.com>
2018-06-24 22:33:31 +0800

    Dijkstra shortest path.

---
+++ src/main/java/com/chenleon/algo/ch4graph/DijkstraSP.java
@@ -0,0 +1,57 @@
+package com.chenleon.algo.ch4graph;
+
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.IndexMinPQ;
+import edu.princeton.cs.algs4.Stack;
+
+public class DijkstraSP {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
+
+    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        pq = new IndexMinPQ<>(G.V());
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0;
+
+        pq.insert(s, distTo[s]);
+
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            for (DirectedEdge e : G.adj(v)) {
+                relax(e);
+            }
+        }
+    }
+
+    private void relax(DirectedEdge e) {
+        int v = e.from(), w = e.to();
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+            if (pq.contains(w)) pq.changeKey(w, distTo[w]);
+            else pq.insert(w, distTo[w]);
+        }
+    }
+
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        Stack<DirectedEdge> stack = new Stack<>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
+            stack.push(e);
+
+        return stack;
+     }
+
+    public boolean hasPathTo(int v) {
+        return edgeTo[v] != null;
+    }
+}
